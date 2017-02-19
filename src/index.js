6bd30fb3ed6178730e3dbcb378a6e36f282d4844
Mike Pham <contact@mikepham.com>
2017-02-18 23:08:02 -0500

    removing cli debugging

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-const process = require('process')
-
-const TwitchStreamer = require('./twitch')
-
-if (process.argv[2]) {
-  switch (process.argv[2]) {
-    case 'twitch':
-      const twitch = new TwitchStreamer(process.argv[3])
-      twitch.start()
-      break;
-  }
-} else {
-  module.exports = {
-    TwitchStreamer: TwitchStreamer
-  }
+module.exports = {
+  TwitchStreamer: require('./twitch')
 }
